47916617d63fbf6c3506ee042a2ca4366f4a1cfd
doputras <ganymedessuper@gmail.com>
2024-12-07 12:33:18 +0700

    7 desember

--- lib/views/animal_inpection/animal_inpection_page.dart
+++ lib/views/animal_inpection/animal_inpection_page.dart
@@ -36,10 +36,11 @@ class _AnimalInpectionPageState extends State<AnimalInpectionPage> {
   final TextEditingController createdAtController = TextEditingController();
 
   final GlobalKey<ChooseAnimalWidgetState> _chooseAnimalKey = GlobalKey();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Future<void> getCamera() async {
     try {
-      pickedFile = await _picker.pickImage(
+      final pickedFile = await _picker.pickImage(
         source: ImageSource.camera,
         maxHeight: 800.0,
         maxWidth: 460.0,
@@ -47,16 +48,21 @@ class _AnimalInpectionPageState extends State<AnimalInpectionPage> {
       );
       if (pickedFile != null) {
         setState(() {
-          tempFile = File(pickedFile!.path);
+          tempFile = File(pickedFile.path);
           bytes = tempFile!.readAsBytesSync();
         });
       }
     } catch (e) {
-      print(e);
+      debugPrint('Error picking image: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Failed to pick an image. Please try again.'),
+          backgroundColor: Colors.red,
+        ),
+      );
     }
   }
 
-  // Function to show DatePicker and set the selected date to the controller
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
       context: context,
@@ -66,7 +72,7 @@ class _AnimalInpectionPageState extends State<AnimalInpectionPage> {
     );
     if (picked != null) {
       setState(() {
-        createdAtController.text = DateFormat('yyyy-MM-dd').format(picked); // Format date as needed
+        createdAtController.text = DateFormat('yyyy-MM-dd').format(picked);
       });
     }
   }
@@ -85,6 +91,58 @@ class _AnimalInpectionPageState extends State<AnimalInpectionPage> {
     super.dispose();
   }
 
+  bool _validateForm() {
+    if (_chooseAnimalKey.currentState?.selectedAnimal == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Please select an animal.'),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return false;
+    }
+    if (createdAtController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Please select a date.'),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return false;
+    }
+    if (!_formKey.currentState!.validate()) {
+      return false;
+    }
+    return true;
+  }
+
+  void _submitForm() {
+    if (_validateForm()) {
+      final selectedAnimal =
+          _chooseAnimalKey.currentState?.selectedAnimal == 'Lainnya'
+              ? _chooseAnimalKey.currentState?.otherAnimal
+              : _chooseAnimalKey.currentState?.selectedAnimal;
+
+      final dataModel = InspectionModel(
+        animal: selectedAnimal ?? '',
+        cageTreatment: cageController.text,
+        environmentalCare: scopeController.text,
+        feeding: eatController.text,
+        inspector: inspectorController.text,
+        location: locationController.text,
+        medicalTreatment: medicalController.text,
+        suggestion: suggestionController.text,
+        result: reviewController.text,
+        date: createdAtController.text,
+      );
+
+      context.read<InspectionBloc>().add(InspectionEvent.addInspection(dataModel));
+      context.read<InspectionInvoiceBloc>().add(
+        InspectionInvoiceEvent.addToListInvoice(dataModel, 'unpaid'),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,199 +156,179 @@ class _AnimalInpectionPageState extends State<AnimalInpectionPage> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              margin: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 231, 229, 229),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: ListView(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                children: [
-                  CustomFormField(
-                    controller: reviewController,
-                    title: 'Hasil pemeriksaan hewan',
-                    fillColor: Colors.white,
-                    maxLine: 3,
-                  ),
-                  const SizedBox(height: 11),
-                  ChooseAnimalWidget(
-                    key: _chooseAnimalKey,
-                    title: 'Pilih Hewan',
-                  ),
-                  const SizedBox(height: 11),
-                  GestureDetector(
-                    onTap: () => _selectDate(context), // Show date picker on tap
-                    child: AbsorbPointer( // Prevents keyboard from appearing
-                      child: CustomFormField(
-                        controller: createdAtController,
-                        title: 'Tanggal pemeriksaan',
-                        fillColor: Colors.white,
-                        maxLine: 1,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                margin: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 231, 229, 229),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: ListView(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  children: [
+                    CustomFormField(
+                      controller: reviewController,
+                      title: 'Hasil pemeriksaan hewan',
+                      fillColor: Colors.white,
+                      maxLine: 3,
+                    ),
+                    const SizedBox(height: 11),
+                    ChooseAnimalWidget(
+                      key: _chooseAnimalKey,
+                      title: 'Pilih Hewan',
+                    ),
+                    const SizedBox(height: 11),
+                    GestureDetector(
+                      onTap: () => _selectDate(context),
+                      child: AbsorbPointer(
+                        child: CustomFormField(
+                          controller: createdAtController,
+                          title: 'Tanggal pemeriksaan',
+                          fillColor: Colors.white,
+                          maxLine: 1,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 11),
-                  CustomFormField(
-                    controller: medicalController,
-                    title: 'Perawatan medis',
-                    fillColor: Colors.white,
-                    maxLine: 2,
-                  ),
-                  const SizedBox(height: 11),
-                  CustomFormField(
-                    controller: cageController,
-                    title: 'Perawatan kandang',
-                    fillColor: Colors.white,
-                    maxLine: 2,
-                  ),
-                  const SizedBox(height: 11),
-                  CustomFormField(
-                    controller: eatController,
-                    title: 'Pemberian makan',
-                    fillColor: Colors.white,
-                    maxLine: 2,
-                  ),
-                  const SizedBox(height: 11),
-                  CustomFormField(
-                    controller: scopeController,
-                    title: 'Perawatan lingkungan',
-                    fillColor: Colors.white,
-                    maxLine: 2,
-                  ),
-                  const SizedBox(height: 11),
-                  CustomFormField(
-                    controller: suggestionController,
-                    title: 'Saran tambahan',
-                    fillColor: Colors.white,
-                    maxLine: 2,
-                  ),
-                  const SizedBox(height: 11),
-                  CustomFormField(
-                    controller: locationController,
-                    title: 'Lokasi',
-                    fillColor: Colors.white,
-                    maxLine: 1,
-                  ),
-                  const SizedBox(height: 11),
-                  CustomFormField(
-                    controller: inspectorController,
-                    title: 'Nama Pemeriksa',
-                    fillColor: Colors.white,
-                    maxLine: 2,
-                  ),
-                  const SizedBox(height: 11),
-                  const Text(
-                    'Bukti Gambar',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-                  ),
-                  const SizedBox(height: 8),
-                  // Image display area
-                  Container(
-                    width: double.infinity,
-                    height: 250,
-                    decoration: BoxDecoration(
-                      color: greyColor.withOpacity(0.7),
-                      borderRadius: BorderRadius.circular(30),
-                      image: tempFile != null
-                          ? DecorationImage(
-                              image: FileImage(tempFile!),
-                              fit: BoxFit.cover,
+                    const SizedBox(height: 11),
+                    CustomFormField(
+                      controller: medicalController,
+                      title: 'Perawatan medis',
+                      fillColor: Colors.white,
+                      maxLine: 2,
+                    ),
+                    const SizedBox(height: 11),
+                    CustomFormField(
+                      controller: cageController,
+                      title: 'Perawatan kandang',
+                      fillColor: Colors.white,
+                      maxLine: 2,
+                    ),
+                    const SizedBox(height: 11),
+                    CustomFormField(
+                      controller: eatController,
+                      title: 'Pemberian makan',
+                      fillColor: Colors.white,
+                      maxLine: 2,
+                    ),
+                    const SizedBox(height: 11),
+                    CustomFormField(
+                      controller: scopeController,
+                      title: 'Perawatan lingkungan',
+                      fillColor: Colors.white,
+                      maxLine: 2,
+                    ),
+                    const SizedBox(height: 11),
+                    CustomFormField(
+                      controller: suggestionController,
+                      title: 'Saran tambahan',
+                      fillColor: Colors.white,
+                      maxLine: 2,
+                    ),
+                    const SizedBox(height: 11),
+                    CustomFormField(
+                      controller: locationController,
+                      title: 'Lokasi',
+                      fillColor: Colors.white,
+                      maxLine: 1,
+                    ),
+                    const SizedBox(height: 11),
+                    CustomFormField(
+                      controller: inspectorController,
+                      title: 'Nama Pemeriksa',
+                      fillColor: Colors.white,
+                      maxLine: 2,
+                    ),
+                    const SizedBox(height: 11),
+                    const Text(
+                      'Bukti Gambar',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                    ),
+                    const SizedBox(height: 8),
+                    // Image display area
+                    Container(
+                      width: double.infinity,
+                      height: 250,
+                      decoration: BoxDecoration(
+                        color: greyColor.withOpacity(0.7),
+                        borderRadius: BorderRadius.circular(30),
+                        image: tempFile != null
+                            ? DecorationImage(
+                                image: FileImage(tempFile!),
+                                fit: BoxFit.cover,
+                              )
+                            : null,
+                      ),
+                      child: tempFile == null
+                          ? const Center(
+                              child: Icon(Icons.image, color: whiteColor, size: 32),
                             )
                           : null,
                     ),
-                    child: tempFile == null
-                        ? const Center(
-                            child: Icon(Icons.image, color: whiteColor, size: 32),
-                          )
-                        : null,
-                  ),
-                  const SizedBox(height: 15),
-                  ElevatedButton(
-                    onPressed: getCamera,
-                    child: const Text("Add Photo"),
-                  ),
-                  const SizedBox(height: 30),
-                  BlocConsumer<InspectionBloc, InspectionState>(
-                    builder: (context, state) {
-                      return state.maybeWhen(
-                        orElse: () {
-                          return InkWell(
-                            onTap: () {
-                              final selectedAnimal =
-                                  _chooseAnimalKey.currentState?.selectedAnimal == 'Lainnya'
-                                      ? _chooseAnimalKey.currentState?.otherAnimal
-                                      : _chooseAnimalKey.currentState?.selectedAnimal;
-
-                              final dataModel = InspectionModel(
-                                animal: selectedAnimal ?? '',
-                                cageTreatment: cageController.text,
-                                environmentalCare: scopeController.text,
-                                feeding: eatController.text,
-                                inspector: inspectorController.text,
-                                location: locationController.text,
-                                medicalTreatment: medicalController.text,
-                                suggestion: suggestionController.text,
-                                result: reviewController.text,
-                                date: createdAtController.text,
-                              );
-                              context.read<InspectionBloc>().add(InspectionEvent.addInspection(dataModel));
-
-                              context.read<InspectionInvoiceBloc>().add(
-                                InspectionInvoiceEvent.addToListInvoice(dataModel, 'unpaid'),
-                              );
-                            },
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(vertical: 15),
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                color: primaryColor,
-                                borderRadius: BorderRadius.circular(10),
+                    const SizedBox(height: 15),
+                    ElevatedButton(
+                      onPressed: getCamera,
+                      child: const Text("Add Photo"),
+                    ),
+                    const SizedBox(height: 30),
+                    BlocConsumer<InspectionBloc, InspectionState>(
+                      builder: (context, state) {
+                        return state.maybeWhen(
+                          orElse: () {
+                            return InkWell(
+                              onTap: _submitForm,
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(vertical: 15),
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  color: primaryColor,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Text(
+                                  'Submit',
+                                  style: whiteTextStyle,
+                                ),
                               ),
-                              child: Text(
-                                'Submit',
-                                style: whiteTextStyle,
+                            );
+                          },
+                          loading: () => const Center(
+                            child: CircularProgressIndicator(color: primaryColor),
+                          ),
+                        );
+                      },
+                      listener: (context, state) {
+                        state.maybeWhen(
+                          orElse: () {},
+                          loaded: (data) {
+                            Navigator.pop(context);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                backgroundColor: greenColor,
+                                content: Text('Add Inspection Success'),
                               ),
-                            ),
-                          );
-                        },
-                        loading: () => const Center(
-                          child: CircularProgressIndicator(color: primaryColor),
-                        ),
-                      );
-                    },
-                    listener: (context, state) {
-                      state.maybeWhen(
-                        orElse: () {},
-                        loaded: (data) {
-                          Navigator.pop(context);
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              backgroundColor: greenColor,
-                              content: Text('Add Inspection Success'),
-                            ),
-                          );
-                        },
-                        error: (message) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              backgroundColor: secondaryColor,
-                              content: Text(message),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  ),
-                ],
+                            );
+                          },
+                          error: (message) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                backgroundColor: secondaryColor,
+                                content: Text(message),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
